aa383f23ea92a4c532f62dd96a55a718ece11489
Axel Rodriguez <alejandrom9712@gmail.com>
2019-04-23 09:42:33 -0600

    Prueba señales

--- Calculadora.vhd
+++ Calculadora.vhd
@@ -21,6 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -33,41 +34,27 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity Calculadora is
 Port (
-    dato1_input: in std_logic_vector(2 downto 0);
-    posicion: in std_logic_vector(2 downto 0);
-    teclaso_mostrar: in std_logic;
-    teclaso_guardar: in std_logic;
-    
-    show_display: out std_logic_vector(7 downto 0);
-    
-    display1: out std_logic;
-    display2: out std_logic;
-    display3: out std_logic;
-    display4: out std_logic
+    clk    : IN  STD_LOGIC;
+    areset : IN  STD_LOGIC;
+    aload  : IN  STD_LOGIC;
+    data   : IN  STD_LOGIC_VECTOR(3 DOWNTO 0);
+    cnt_out: OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
 );
 end Calculadora;
 
 architecture Behavioral of Calculadora is
+signal cnt_tmp: STD_LOGIC_VECTOR(3 DOWNTO 0) := "0000";
 begin
-display1 <= '0';
-display2 <= '1';
-display3 <= '1';
-display4 <= '1';
-
-process (teclaso_guardar, teclaso_mostrar)
-    begin
-        if teclaso_guardar = '1' then
-            case (dato1_input) is
-                when "000" => show_display <= "00000001";
-                when "001" => show_display <= "00000011";
-                when "010" => show_display <= "00000111";
-                when "011" => show_display <= "00001111";
-                when "100" => show_display <= "00011111";
-                when "101" => show_display <= "00111111";
-                when "110" => show_display <= "01111111";
-                when "111" => show_display <= "11111111";
-            end case;
+    proceso_contador: process (aload, areset, clk, data) begin
+        if areset = '1' then
+            cnt_tmp <= "0000";
+        elsif aload = '1' then
+            cnt_tmp <= data;
+        elsif rising_edge(clk) then
+            cnt_tmp <= cnt_tmp + 1;
         end if;
-end process;
+    end process;
+ 
+    cnt_out <= cnt_tmp;
 
 end Behavioral;
