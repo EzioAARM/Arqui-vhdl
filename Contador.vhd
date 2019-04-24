d9778697909ab658ba12f48e188362829b7ef41b
Axel Rodriguez <alejandrom9712@gmail.com>
2019-04-24 07:17:26 -0600

    Prueba 2

--- Contador.vhd
+++ Contador.vhd
@@ -84,19 +84,19 @@ end process;
 process(act)
 begin
     case act is
-    when "00" =>
-        Display <= "0111"; 
-        SDDisplay <= numero(15 downto 12);
-    when "01" =>
-        Display <= "1011"; 
-        SDDisplay <= numero(11 downto 8);
-    when "10" =>
-        Display <= "1101"; 
-        SDDisplay <= numero(7 downto 4);
-    when "11" =>
-        Display <= "1110"; 
-        SDDisplay <= numero(3 downto 0);
-
+        when "00" =>
+            Display <= "0111"; 
+            SDDisplay <= numero(15 downto 12);
+        when "01" =>
+            Display <= "1011"; 
+            SDDisplay <= numero(11 downto 8);
+        when "10" =>
+            Display <= "1101"; 
+            SDDisplay <= numero(7 downto 4);
+        when "11" =>
+            Display <= "1110"; 
+            SDDisplay <= numero(3 downto 0);
+        when others => Display <= "0000";
     end case;
 end process;
 
