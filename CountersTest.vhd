6d790c1392b7b7540b6a90b21eb605289a9fa8f4
Axel Rodriguez <alejandrom9712@gmail.com>
2019-05-02 00:28:52 -0600

    Pruebas con timer w5

--- CountersTest.vhd
+++ CountersTest.vhd
@@ -34,7 +34,7 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity CountersTest is
     Port (
-        clk : in std_logic;
+        btn : in std_logic;
         display : out std_logic_vector(3 downto 0);
         cot_out : out std_logic_vector(6 downto 0)
     );
@@ -43,33 +43,31 @@ end CountersTest;
 architecture Behavioral of CountersTest is
 
 signal num : integer;
---signal clk : std_logic := '0';
 
 begin
-display <= "1110";
---clk <= not clk after 500 ms;
-process (clk) is
-    begin
-        if (rising_edge(clk)) then
-            if (num = 9) then
-                num <= 0;
-            else 
-                num <= num + 1;
+    display <= "1110";
+    process (btn) is
+        begin
+            if (btn = '1') then
+                if (num = 9) then
+                    num <= 0;
+                else 
+                    num <= num + 1;
+                end if;
+                case num is
+                    when 0 =>  cot_out <= "0000001"; -- 0
+                    when 1 =>  cot_out <= "1001111"; -- 1
+                    when 2 =>  cot_out <= "0010010"; -- 2
+                    when 3 =>  cot_out <= "0000110"; -- 3
+                    when 4 =>  cot_out <= "1001100"; -- 4
+                    when 5 =>  cot_out <= "0100100"; -- 5
+                    when 6 =>  cot_out <= "1100000"; -- 6
+                    when 7 =>  cot_out <= "0001111"; -- 7
+                    when 8 =>  cot_out <= "0000000"; -- 8
+                    when 9 =>  cot_out <= "0000100"; -- 9
+                    when others => cot_out <= "0000001";
+                end case;
             end if;
-            case num is
-                when 0 =>  cot_out <= "0000001"; -- 0
-                when 1 =>  cot_out <= "1001111"; -- 1
-                when 2 =>  cot_out <= "0010010"; -- 2
-                when 3 =>  cot_out <= "0000110"; -- 3
-                when 4 =>  cot_out <= "1001100"; -- 4
-                when 5 =>  cot_out <= "0100100"; -- 5
-                when 6 =>  cot_out <= "1100000"; -- 6
-                when 7 =>  cot_out <= "0001111"; -- 7
-                when 8 =>  cot_out <= "0000000"; -- 8
-                when 9 =>  cot_out <= "0000100"; -- 9
-                when others => cot_out <= "0000001";
-            end case;
-        end if;
-    end process;
+        end process;
 
 end Behavioral;
